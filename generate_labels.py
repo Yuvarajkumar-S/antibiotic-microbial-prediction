2b58be63974a71acd640582cfd92e62b5475c890
Yuvarajkumar-S <yuvarajsankar000@gmail.com>
2026-03-13 22:29:08 +0530

    Update generate_labels.py

--- generate_labels.py
+++ generate_labels.py
@@ -1,18 +1,31 @@
 from Bio import SeqIO
 import pandas as pd
-import random
 
 samples = []
+labels = []
 
 for record in SeqIO.parse("bacteria.10.2.genomic.fna", "fasta"):
+    seq = str(record.seq)
 
-    samples.append({
-        "sample": record.id,
-        "resistance": random.randint(0,1)
-    })
+    # count GC content
+    gc_count = seq.count("G") + seq.count("C")
+    gc_ratio = gc_count / len(seq)
 
-df = pd.DataFrame(samples)
+    # create labels based on GC content
+    if gc_ratio > 0.5:
+        label = 1
+    else:
+        label = 0
+
+    samples.append(record.id)
+    labels.append(label)
+
+df = pd.DataFrame({
+    "sample": samples,
+    "resistance": labels
+})
 
 df.to_csv("labels.csv", index=False)
 
-print("labels.csv generated successfully")
+print("labels.csv created successfully")
+print(df["resistance"].value_counts())
